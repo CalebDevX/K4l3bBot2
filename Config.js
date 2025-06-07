38a3952fe62daba5eed0705b7a547a71446b9427
K4l3b <143396031+CalebDevX@users.noreply.github.com>
2025-06-07 21:47:28 +0100

    Config.js

--- Config.js
+++ Config.js
@@ -3,15 +3,15 @@ const fs = require('fs');
 const chalk = require('chalk');
 
 // Contact details
-global.ownernomer = process.env.OWNER_NOMER || "2348109263390";
-global.ytname = process.env.YT_NAME || "YT: @EliteProTech";
-global.socialm = process.env.SOCIAL_M || "GitHub: EliteProTech";
-global.location = process.env.LOCATION || "Nigeria, Port Harcourt";
+global.ownernomer = process.env.OWNER_NOMER || "2348096567473";
+global.ytname = process.env.YT_NAME || "YT: @Calebosky";
+global.socialm = process.env.SOCIAL_M || "GitHub: CalebDevX";
+global.location = process.env.LOCATION || "Nigeria, Abuja";
 
 // Creator details
-global.ownernumber = process.env.OWNER_NUMBER || '2348109263390';
-global.ownername = process.env.OWNER_NAME || 'ElitePro';
-global.botname = process.env.BOT_NAME || 'ELITE-PRO-V1';
+global.ownernumber = process.env.OWNER_NUMBER || '2348096567473';
+global.ownername = process.env.OWNER_NAME || 'Caleb';
+global.botname = process.env.BOT_NAME || 'K4l3bBot';
 
 // Default settings 
 global.prefa = process.env.PREFIX ? process.env.PREFIX.split(',') : ['', '!', '.', '#', '&'];
@@ -23,27 +23,27 @@ global.autoread = process.env.AUTO_READ === 'true';
 global.autobio = process.env.AUTO_BIO !== 'false'; // default true
 global.anti92 = process.env.ANTI_92 === 'true';
 global.autoswview = process.env.AUTO_SW_VIEW !== 'false'; // default true
-global.welcome = process.env.WELCOME !== 'false'; // default true
+global.welcome = process.env.WELCOME !== 'true'; // default true
 global.autoreact = process.env.AUTO_REACT === 'true';
-global.autolikestatus = process.env.AUTO_LIKE_STATUS === 'true';
+global.autolikestatus = process.env.AUTO_LIKE_STATUS === 'false';
 
 // Thumbnail profile picture
-global.elitepropp = process.env.ELITE_PRO_PP || 'https://i.ibb.co/jk0ynvbn/7a8c4b5b617fa11a1e9a61190f427546.jpg';
+global.elitepropp = process.env.ELITE_PRO_PP || 'https://i.ibb.co/rK16XG8N/IMG-2347.jpg';
 // Default emoji
 global.themeemoji = process.env.THEME_EMOJI || '👨‍💻';
 
 
 // Sticker details
 global.packname = process.env.PACKNAME || 'Sticker By';
-global.author = process.env.AUTHOR || 'ELITEPRO\n\nContact: +2348109263390';
+global.author = process.env.AUTHOR || 'ELITEPRO\n\nContact: +2348096567473';
 // Default settings 2
 global.typemenu = process.env.TYPE_MENU || 'v2';
-global.wm = process.env.WM || "Youtube @EliteProTech";
-global.link = process.env.LINK || 'https://whatsapp.com/channel/0029VaXaqHII1rcmdDBBsd3g';
+global.wm = process.env.WM || "Youtube @Calebosky";
+global.link = process.env.LINK || 'https://whatsapp.com/channel/0029VbAfyp57dmeXh5trtY2W';
 
 // Text bug
 global.xbugtex = {
-    xtxt: '👨‍💻ELITE-PRO👨‍💻',
+    xtxt: '👨‍💻K4l3b👨‍💻',
 };
 
 // Reply messages
